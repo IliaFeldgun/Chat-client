7d198c43561dfb8c9a3de7e94f92113b2b0c5da8
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-30 20:34:59 +0300

    Sends message to socket

--- src/components/Chat/MessageInput.jsx
+++ src/components/Chat/MessageInput.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Button, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+import Socket from '../../api/socket'
+
 const useStyles = makeStyles((theme) => ({
     messageInput: {
         // order: 2
@@ -14,15 +16,25 @@ const MessageInput = () => {
     const handleChange = (event) => {
         changeInput(event.target.value)
     }
+
     const sendMessage = (message) => {
-        // Send to server, useEffect
+        Socket.sendMessage(message)
+        changeInput("")
     }
+
     const handleSubmit = (event) => {
+        event.preventDefault()
         sendMessage(input)
     }
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter')
+            sendMessage(input)
+    }
+    
     return (
         <div className={classes.messageInput}> 
-            <TextField variant="outlined" onChange={handleChange} onSubmit={handleSubmit}/>
+            <TextField variant="outlined" onChange={handleChange} onKeyPress={handleKeyPress}/>
             <Button onClick={handleSubmit}>
                 {">"}
             </Button>
