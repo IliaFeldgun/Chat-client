3a4a56acaf14fe5ceb83adf7f395aaeb83545302
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 21:34:23 +0300

    Wrapping and position changes

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import { ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core'
-
+const useStyles = makeStyles((theme) => ({
+    message: {
+        overflowWrap: 'break-word',
+    },
+    avatar: {
+        alignSelf: 'flex-end'
+    }
+}))
 const Message = ({userName, message, timestamp}) => {
+    const classes = useStyles()
+
     return (
         <ListItem>
-            <ListItemAvatar>
-                <Avatar alt={userName} src={`https://picsum.photos/seed/${userName}/100`}/>
+            <ListItemAvatar className={classes.avatar}>
+                <Avatar 
+                    alt={userName} 
+                    src={`https://picsum.photos/seed/${userName}/100`}
+                />
             </ListItemAvatar>
-            <ListItemText primary={userName} secondary={message} />
-            {(new Date(timestamp)).toLocaleString()}
+            <ListItemText 
+                className={classes.message} 
+                primary={message} 
+                secondary={
+                    `${userName} at ${(new Date(timestamp)).toLocaleString()}`
+                } 
+            />
         </ListItem>
     )
 }
