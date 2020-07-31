9ea586754c091216a13797dabc8786360b1e0200
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 16:20:54 +0300

    Username got from socket

--- src/components/Header/TopBar.jsx
+++ src/components/Header/TopBar.jsx
@@ -3,7 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, Button} from '@material-ui/core'
 
 import Login from './Login'
+import Socket from '../../api/socket'
 import SessionStorage from '../../engine/SessionStorage'
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -24,9 +26,16 @@ const TopBar = () => {
         setState({ ...state, loginOpen })
     }
     const handleLoginClose = () => {
-        initUserName()
         toggleModal(false)
     }
+    
+    const handleLoggedIn = (user) => {
+        SessionStorage.setUserName(user.userName)
+        
+        initUserName()
+    }
+    Socket.registerToLogin(handleLoggedIn)
+
     const initUserName = () => {
         const userName = SessionStorage.getUserName()
         if (userName) {
