e566bff759f0d080de57652c2ba9361fc559b6d0
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 16:13:18 +0300

    Message display component

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
+import Message from './Message'
+
 const useStyles = makeStyles((theme) => ({
     messages: {
         //order: 1
@@ -14,7 +16,12 @@ const Messages = ({messages}) => {
         <div className={classes.messages}>
             {
                 messages.map((message) => {
-                    return <div key={message.message}>{message.message}</div>
+                    return <Message 
+                        key={message.timestamp + message.userName}
+                        message={message.message}
+                        userName={message.userName}
+                        timestamp={message.timestamp}
+                    />
                 })
             }
         </div>
