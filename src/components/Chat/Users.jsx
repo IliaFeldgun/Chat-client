1811134352cb165913733f7f18d9a478138c3282
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 19:41:31 +0300

    Design for users

--- src/components/Chat/Users.jsx
+++ src/components/Chat/Users.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { 
+    List, ListItem, Chip, ListItemAvatar, Avatar 
+} from '@material-ui/core'
 
 const Users = ({users}) => {
     return (
-        users.map((user) => {
-            return <div key={user}>{user}</div>
-        })
+        <List>
+            {
+                users.map((user) => {
+                    return (
+                        <ListItem key={user}>
+                            <Chip 
+                                label={user} 
+                                variant="outlined" 
+                                size="small" 
+                                avatar={<Avatar>{user[0]}</Avatar>} 
+                            />
+                        </ListItem>
+                    )
+                })
+            }
+        </List>
     )
 }
 
