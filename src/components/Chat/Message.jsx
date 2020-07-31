ef084f638a76756366d2e3216042856e722588e6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-31 18:36:30 +0300

    Time localized

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Message = ({userName, message, timestamp}) => {
     return (
         <div>
-            {userName}: {message} at {timestamp}
+            {userName}: {message} at {(new Date(timestamp)).toLocaleString()}
         </div>
     )
 }
