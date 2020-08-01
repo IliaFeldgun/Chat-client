7a34ff38cfa663b4fb1cfc8de01b77d868a9ac6a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-01 21:26:10 +0300

    Modal for errors

---
+++ src/components/General/ErrorBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Dialog, DialogTitle, DialogContent} from '@material-ui/core'
+import Socket from '../../api/socket'
+
+const ErrorBox = () => {
+    const [open, setOpen] = React.useState(false)
+    const [error, setError] = React.useState('')
+
+    React.useEffect(() => {
+        Socket.registerToError((error) => {
+            setError(error)
+            setOpen(true)
+        })
+    }, [open, error])
+
+    const toggleDialog = () => {
+        onClose()
+    }
+    return (
+        <Dialog open={open} onClose={() => toggleDialog(false)}>
+            <DialogTitle>Error</DialogTitle>
+            <DialogContent>
+                {error}
+            </DialogContent>
+        </Dialog>
+    )
+}
+export default ErrorBox
